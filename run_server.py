9d6178836675fde544302cd1d2af26e96cd4dec6
Nika254 <abelcq02@gmail.com>
2025-10-26 18:24:50 +0000

    chore(pre-commit): add config and docs; apply auto-format fixes

--- run_server.py
+++ run_server.py
@@ -1,7 +1,9 @@
-from app import app
-import sys
 import os
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), 'src'))
+import sys
 
-if __name__ == '__main__':
-    app.run(host='127.0.0.1', port=3001, debug=False, use_reloader=False)
+from app import app
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "src"))
+
+if __name__ == "__main__":
+    app.run(host="127.0.0.1", port=3001, debug=False, use_reloader=False)
