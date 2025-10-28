cf4ceb91a9b206b9f5cf7ed2c2826e686346f0d8
Nika254 <abelcq02@gmail.com>
2025-10-28 08:24:09 +0000

    chore(models): remove comments from models.py

--- src/api/models.py
+++ src/api/models.py
@@ -1,19 +1,27 @@
+import datetime
+
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import Boolean, String
-from sqlalchemy.orm import Mapped, mapped_column
 
 db = SQLAlchemy()
 
 
 class User(db.Model):
-    id: Mapped[int] = mapped_column(primary_key=True)
-    email: Mapped[str] = mapped_column(String(120), unique=True, nullable=False)
-    password: Mapped[str] = mapped_column(nullable=False)
-    is_active: Mapped[bool] = mapped_column(Boolean(), nullable=False)
+    __tablename__ = "user"
+
+    id = db.Column(db.Integer, primary_key=True)
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
+    is_active = db.Column(db.Boolean(), nullable=False, default=True)
 
     def serialize(self):
-        return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
-        }
+        return {"id": self.id, "email": self.email}
+
+
+class RevokedToken(db.Model):
+    __tablename__ = "revoked_token"
+
+    id = db.Column(db.Integer, primary_key=True)
+    jti = db.Column(db.String(64), unique=True, nullable=False)
+    created_at = db.Column(
+        db.DateTime, nullable=False, default=datetime.datetime.utcnow
+    )
